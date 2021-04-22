8d1875a3629e5792a68b7cbcb529b11aa3cdd413
Klemek <klemek@outlook.fr>
2021-04-22 16:22:26 +0200

    top arg for %find

--- src/scanners/find_scanner.py
+++ src/scanners/find_scanner.py
@@ -23,6 +23,7 @@ class FindScanner(Scanner):
             "find",
             "Find specific words or phrases (you can use quotes to add spaces in queries)",
             args=[
+                "top - rank users for these queries",
                 "all/everyone - include bots",
             ],
             example='#mychannel1 #mychannel2 @user "I love you" "you too"',
@@ -31,7 +32,7 @@ class FindScanner(Scanner):
     def __init__(self):
         super().__init__(
             all_args=True,
-            valid_args=["all", "everyone"],
+            valid_args=["all", "everyone", "top"],
             help=FindScanner.help(),
             intro_context="Matches",
         )
@@ -39,6 +40,7 @@ class FindScanner(Scanner):
     async def init(self, message: discord.Message, *args: str) -> bool:
         self.matches = defaultdict(Counter)
         self.all_messages = "all" in args or "everyone" in args
+        self.top = "top" in args
         if len(self.other_args) == 0:
             await message.channel.send(
                 "You need to add a query to find (you can use quotes to add spaces in queries)",
@@ -54,27 +56,38 @@ class FindScanner(Scanner):
             self.other_args,
             self.raw_members,
             all_messages=self.all_messages,
+            top=self.top,
         )
 
     def get_results(self, intro: str) -> List[str]:
+        res = [intro]
         matches = [match for match in self.matches]
         matches.sort(key=lambda match: self.matches[match].score(), reverse=True)
         usage_count = Counter.total(self.matches)
-        res = [intro]
-        res += [
-            self.matches[match].to_string(
-                matches.index(match),
-                f'"{escape_text(match)}"'
-                if not match.startswith("<:")
-                else escape_text(match),
-                total_usage=self.msg_count,
-                ranking=False,
-                transform=lambda id: f" by {mention(id)}",
-                top=len(self.members) != 1,
-            )
-            for match in matches
-        ]
-        if len(matches) > 1:
+        if self.top:
+            res += [
+                self.matches[match].to_string(
+                    matches.index(match),
+                    mention(match),
+                    total_usage=usage_count,
+                )
+                for match in matches
+            ]
+        else:
+            res += [
+                self.matches[match].to_string(
+                    matches.index(match),
+                    f'"{escape_text(match)}"'
+                    if not match.startswith("<:")
+                    else escape_text(match),
+                    total_usage=self.msg_count,
+                    ranking=False,
+                    transform=lambda id: f" by {mention(id)}",
+                    top=len(self.members) != 1,
+                )
+                for match in matches
+            ]
+        if self.top or len(matches) > 1:
             res += [
                 f"Total: {plural(usage_count,'time')} ({precise(usage_count/self.msg_count)}/msg)"
             ]
@@ -90,6 +103,7 @@ class FindScanner(Scanner):
         raw_members: List[int],
         *,
         all_messages: bool,
+        top: bool,
     ) -> bool:
         impacted = False
         # If author is included in the selection (empty list is all)
@@ -101,7 +115,10 @@ class FindScanner(Scanner):
             impacted = True
             content = message.content.lower()
             for query in queries:
-                matches[query].update_use(
-                    content.count(query.lower()), message.created_at, message.author
-                )
+                count = content.count(query.lower())
+                if top:
+                    if count > 0:
+                        matches[message.author].update_use(count, message.created_at)
+                else:
+                    matches[query].update_use(count, message.created_at, message.author)
         return impacted
